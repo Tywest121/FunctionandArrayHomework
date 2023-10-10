622143315d9cac3adbe5b43e3784e368511dccbf
tywest121 <tyresewest25@gmail.com>
2023-10-10 17:03:15 -0400

    Ready

---
+++ test.js
@@ -0,0 +1,23 @@
+var a= 12
+var b= 20
+
+
+function football(num1,num2){
+    return num1 + num2
+}
+console.log(football(a,b))
+
+function hockey(num1,num2){
+    return num1 - num2
+}
+console.log(hockey(a,b))
+
+function golf(param1,param2){
+    return param1 * param2
+}
+console.log(golf(a,b))
+
+function soccer(param1,param2){
+    return param1 / param2
+}
+console.log(soccer(a,b))
